5b6ac01839a9899f09da438af10fb735d0886c59
KJW <48905932+jiwonkim0131@users.noreply.github.com>
2024-11-21 21:54:33 +0900

    1주차 - 문제7

---
+++ 1주차/7.js
@@ -0,0 +1,38 @@
+// 문제7. 방문 길이
+const solution = (dirs) => {
+  // 다음 좌표 계산
+  const nextLocation = (x, y, dir) => {
+    // const dirs = [[-1, 0], [0, 1], [1, 0], [0, -1]];
+    switch (dir) {
+      case "U":
+        return [x, y + 1];
+      case "D":
+        return [x, y - 1];
+      case "L":
+        return [x - 1, y];
+      case "R":
+        return [x + 1, y];
+    }
+  };
+
+  // 처음에는 (0, 0)에서 시작
+  let x = 0;
+  let y = 0;
+  const visited = new Set();
+  for (const dir of dirs) {
+    const [nx, ny] = nextLocation(x, y, dir);
+
+    // 좌표 안에 있는지 확인
+    if (nx >= -5 && nx <= 5 && ny >= -5 && ny <= 5) {
+      visited.add(`${x}${y}${nx}${ny}`);
+      visited.add(`${nx}${ny}${x}${y}`); // 반대 경로도 같이 등록해줘야 중복이 제거됨
+    }
+
+    x = nx;
+    y = ny;
+  }
+
+  return visited.size / 2;
+};
+
+console.log(solution("ULLURRDLLU"));
